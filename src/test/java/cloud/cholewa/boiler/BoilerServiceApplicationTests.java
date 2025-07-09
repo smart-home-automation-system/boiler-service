d6b2b2886a2a609db27958f56b3bd2bedfdcce37
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-07-09 20:52:16 +0200

    initial commit

--- src/test/java/cloud/cholewa/boiler/BoilerServiceApplicationTests.java
+++ src/test/java/cloud/cholewa/boiler/BoilerServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package cloud.cholewa.water;
+package cloud.cholewa.boiler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WaterServiceApplicationTests {
+class BoilerServiceApplicationTests {
 
     @Test
     void contextLoads() {
