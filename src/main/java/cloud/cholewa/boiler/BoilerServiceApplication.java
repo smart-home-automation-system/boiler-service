a74e45680c55345d4a7b7b25c914987f97cae0c9
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-07-09 20:45:37 +0200

    initial commit

---
+++ src/main/java/cloud/cholewa/boiler/BoilerServiceApplication.java
@@ -0,0 +1,12 @@
+package cloud.cholewa.boiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilerServiceApplication.class, args);
+    }
+}
