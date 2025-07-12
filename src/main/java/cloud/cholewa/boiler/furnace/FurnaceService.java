63475f58d804d0c076386fdda7679e1c2c460d5a
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-07-12 23:46:08 +0200

    added furnace control

--- src/main/java/cloud/cholewa/boiler/furnace/FurnaceService.java
+++ src/main/java/cloud/cholewa/boiler/furnace/FurnaceService.java
@@ -1,31 +1,77 @@
 package cloud.cholewa.boiler.furnace;
 
-import com.rabbitmq.client.Channel;
+import cloud.cholewa.boiler.config.BoilerConfig;
+import cloud.cholewa.boiler.model.DeviceRabbitMessage;
+import cloud.cholewa.boiler.model.DeviceStatus;
+import cloud.cholewa.boiler.model.DeviceType;
+import cloud.cholewa.boiler.model.LastMessage;
+import cloud.cholewa.boiler.shelly.ShellyClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+import static cloud.cholewa.boiler.model.DeviceType.FURNACE;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class FurnaceService {
 
-    @RabbitListener(queues = "furnace.status", id = "asd")
-    public void test(final Message message, final Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) {
+    private final BoilerConfig boiler;
+    private final ShellyClient shellyClient;
 
-        log.info(
-            "Received Furnace Status Message from RabbitMQ message: {}",
-            new String(message.getBody(), StandardCharsets.UTF_8)
-        );
-        log.info("Received Furnace Status Message from RabbitMQ from channel {}", channel.toString());
-        log.info("Received Furnace Status Message from RabbitMQ from tag {}", tag);
+    @RabbitListener(queues = "furnace")
+    void test(final DeviceRabbitMessage body) {
+
+        if (DeviceType.valueOf(body.getName().toUpperCase()).equals(FURNACE)) {
+            log.info("Incoming device message received furnace, enabled:{}", body.isEnabled());
+
+            Mono.just(boiler.getFurnace())
+                .flatMap(this::canFurnaceStart)
+                .doOnNext(furnace -> updateDeviceStatus(furnace, body))
+                .zipWith(shellyClient.controlFurnace(body.isEnabled()))
+                .map(t -> {
+                    t.getT1().setWorking(Boolean.TRUE.equals(t.getT2().getIson()));
+                    return t.getT1();
+                })
+                .subscribe(
+                    furnace -> {
+                    },
+                    error -> log.error("Errors handling furnace: {}", error.getMessage())
+                )
+            ;
+        }
+
     }
 
+    private Mono<DeviceStatus> canFurnaceStart(final DeviceStatus furnaceStatus) {
+        return Mono.just(furnaceStatus)
+            .flatMap(status -> shellyClient.getHotWaterPumpStatus()
+                .flatMap(response -> {
+                    if (Boolean.TRUE.equals(response.getIson())) {
+                        return Mono.just(status);
+                    }
+                    return Mono.empty();
+                })
+            )
+            .flatMap(status -> shellyClient.getHeatingPumpStatus()
+                .flatMap(response -> {
+                    if (Boolean.TRUE.equals(response.getIson())) {
+                        return Mono.just(status);
+                    }
+                    return Mono.empty();
+                })
+            );
+    }
 
+    private void updateDeviceStatus(final DeviceStatus device, final DeviceRabbitMessage body) {
+        device.setLastMessage(LastMessage.builder()
+            .timestamp(LocalDateTime.now())
+            .message(body.toString())
+            .build());
+    }
 }
